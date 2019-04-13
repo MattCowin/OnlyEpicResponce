322e1fe327b1ecc2163e2145c74ed4760ad22cf5
matt <mattcowin82@gmail.com>
2019-04-13 13:25:04 -0400

    New driver connection

--- src/main/java/com/revature/app/Home.java
+++ src/main/java/com/revature/app/Home.java
@@ -1,5 +1,10 @@
 package com.revature.app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import com.revature.dao.UserDAO;
@@ -7,14 +12,28 @@ import com.revature.model.DBUser;
 
 public class Home {
 
-	public static void main(String[] args) {
 		
+	public static void main(String[] args) throws SQLException, ClassNotFoundException{
 		
-		UserDAO userdao = new UserDAO();
-		List<DBUser> list = userdao.getAllUsersById("");
-		for(int i= 0; i< list.size(); i++) {
-			System.out.println(list.get(i));
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "Matt", "Dumb0");
+			Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs = stmt.executeQuery("SELECT * FROM REIMBURSEMENTS_REQUEST");
+			rs.last();			
+			System.out.println("number of rows " + rs.getRow());
+		} catch (SQLException e) {
+			System.err.println(e);
+			e.printStackTrace();
 		}
+	
+//		UserDAO userdao = new UserDAO();
+//		List<DBUser> list = userdao.getAllUsersById("");
+//		for(int i= 0; i< list.size(); i++) {
+//			System.out.println(list.get(i));
+//		}
 
 		
 		
