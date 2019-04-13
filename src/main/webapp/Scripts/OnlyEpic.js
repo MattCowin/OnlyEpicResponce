44673ca1722d067c6f020cec1f9893f90ad0fc55
matt <mattcowin82@gmail.com>
2019-04-13 10:34:31 -0400

    10:34am 4/13

--- src/main/webapp/Scripts/OnlyEpic.js
+++ src/main/webapp/Scripts/OnlyEpic.js
@@ -23,24 +23,24 @@ function get(url){
 	}
 	)
 }
-
-//function getPolkamanMasters(){
-//	let url = "../GetPolkamans";
-//	
-//	let selectBox = document.getElementById("selectBox");
-//	let results = get(url).then(data => {
-//		console.log(data);
-//		//enhanced for loop in JavaScript
-//		for(let d in data){
-//			let option = document.createElement("option");
-//			option.innerHTML = data[d]["name"];
-//			option.value = data[d]["id"];
-//			selectBox.append(option);
-//		}
-//	}).catch(error =>{
-//		console.lot("Hey, where's my data?")
-//	});
-//}
+//Need this function to populate the reimbursments form
+function getReimbursmentRequest(){
+	let url = "../reimbursments.html";
+	
+	let selectBox = document.getElementById("selectBox");
+	let results = get(url).then(data => {
+		console.log(data);
+		//enhanced for loop in JavaScript
+		for(let d in data){
+			let option = document.createElement("option");
+			option.innerHTML = data[d]["name"];
+			option.value = data[d]["id"];
+			selectBox.append(option);
+		}
+	}).catch(error =>{
+		console.lot("Hey, where's my data?")
+	});
+}
 
 function backTo(){
 	document.location.replace("../index.html");
